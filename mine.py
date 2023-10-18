8ccdd09e75b12921676e5cc11ec98dd7b4635eaf
MAC <liadioluwaseun@gmail.com>
2023-10-18 09:46:57 +0100

    fixed name and email bug

--- mine.py
+++ mine.py
@@ -1,6 +1,6 @@
 # Define variables
-name = "PUT YOUR NAME"
-email = 'PUT YOUR EMAIL'
+name = "Liadi Ismail Oluwaseun"
+email = 'liadioluwaseun@gmail.com'
 
 # Print the name and email address
 print("Name:", name)
